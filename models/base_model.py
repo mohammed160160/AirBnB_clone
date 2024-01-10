4d0b9b1b74745d6e59586f47b7f6005fa62d0a61
iyaz07 <moseseniade@gmail.com>
2024-01-10 07:05:13 -0800

    BaseModel Testes Ok, Task3

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""
+"""
+from datetime import datetime
+import uuid
+
+class BaseModel:
+    """
+    """
+    def __init__(self):
+        """
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+
+    def save(self):
+        """
+        """
+        self.updated_at = datetime.utcnow()
+
+    def to_dict(self):
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
+
+    def __str__(self):
+        """
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
