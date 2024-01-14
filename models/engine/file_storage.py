43fa035528ad1d5a3b2c490054669cebcd6512de
iyaz07 <moseseniade@gmail.com>
2024-01-13 22:44:59 -0800

    Modify filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,7 +25,7 @@ class FileStorage:
         """
         Returns the __objects dictionary
         """
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """
@@ -33,15 +33,15 @@ class FileStorage:
         <obj class name>.id.
         """
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """
         Serializes the __objects dictionary into
         JSON format and saves it to the file specified by __file_path.
         """
-        obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
-        with open(self.__file_path, 'w', encoding="utf-8") as file:
+        obj_dict = {key: obj.to_dict() for key, obj in FileStorage.__objects.items()}
+        with open(FileStorage.__file_path, 'w', encoding="utf-8") as file:
             json.dump(obj_dict, file)
 
     def reload(self):
@@ -55,6 +55,6 @@ class FileStorage:
                     for key, value in obj_dict.items():
                         class_name, obj_id = key.split('.')
                         obj = eval(class_name)(**value)
-                        self.__objects[key] = obj
+                        FileStorage.__objects[key] = obj
                 except Exception:
                     pass
