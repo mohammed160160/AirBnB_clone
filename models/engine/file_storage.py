0cc44e01db725d33e6bc4a2782212692b28aa0dc
iyaz07 <moseseniade@gmail.com>
2024-01-13 18:40:38 -0800

    Add doc

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+Modeule for serializing and deserializing data
+"""
 import json
 import os
 from models.base_model import BaseModel
@@ -8,25 +11,33 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+
+
 class FileStorage:
     """
+    FileStorage class to store, serialize and deserialize
     """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
         """
+        Returns the __objects dictionary
         """
         return self.__objects
 
     def new(self, obj):
         """
+        Sets an object in the __objects dictionary using
+        <obj class name>.id.
         """
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
         """
+        Serializes the __objects dictionary into
+        JSON format and saves it to the file specified by __file_path.
         """
         obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
         with open(self.__file_path, 'w', encoding="utf-8") as file:
@@ -36,12 +47,13 @@ class FileStorage:
         """
         Deserializes the JSON file to __objects.
         """
-        try:
+        if os.path.isfile(FileStorage.__file_path):
             with open(self.__file_path, 'r', encoding="utf-8") as file:
-                obj_dict = json.load(file)
-                for key, value in obj_dict.items():
-                    class_name, obj_id = key.split('.')
-                    obj = eval(class_name)(**value)
-                    self.__objects[key] = obj
-        except:
-            pass
+                try:
+                    obj_dict = json.load(file)
+                    for key, value in obj_dict.items():
+                        class_name, obj_id = key.split('.')
+                        obj = eval(class_name)(**value)
+                        self.__objects[key] = obj
+                except Exception:
+                    pass
